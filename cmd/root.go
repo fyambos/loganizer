aaa17db2de174ed1477afa23ce030ef639bc087e
fyambos <feliciteyambos@gmail.com>
2025-09-24 19:46:43 +0200

    set up de la commande racine de cobra

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,25 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
-// rootCmd représente la commande de base lorsqu'elle est appelée sans sous-commande.
-// C'est une variable globale (dans le package cmd) qui est un pointeur vers une instance de cobra.Command.
-
 var rootCmd = &cobra.Command{
-	Use:   "loganizer",                                                                                       // Définit comment la commande principale doit être appelée (ex: 'gowatcher').
-	Short: "Gowatcher est un outil pour vérifier l'accessibilité des URLs.",                                  // Une courte description de la commande, affichée dans l'aide.
-	Long:  `Un outil CLI en Go pour vérifier l'état d'URLs, gérer la concurrence et exporter les résultats.`, // Une description plus longue, affichée avec 'gowatcher help'.
-	// Run: func(cmd *cobra.Command, args []string) {}, // Pas de Run direct pour la root si elle a des sous-commandes
+	Use:   "loganizer",
+	Short: "loganizer est un outil pour analyser des logs.",
+	Long:  `Un outil cli pour analyser des logs en parallele et exporter les résultats.`,
+}
 
-	// Execute ajoute toutes les commandes enfants à la commande racine et définit également les drapeaux.
-	// C'est appelé par main.main(). Il n'y a qu'une seule fois Execute.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Erreur: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+
 }
